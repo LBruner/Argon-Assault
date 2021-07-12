a673ea24d6ecd66164ec05733bc59c41f784d8a4
LBruner <leandrobruner15@gmail.com>
2021-07-12 15:38:52 -0300

    code refactor

---
+++ Argon Assault/Assets/Scripts/Core/CollisionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [Tooltip("In seconds")][SerializeField] float loadLevelDelay;
+
+    [Tooltip("Gameobject")][SerializeField] GameObject explosionSFX;
+
+    bool isCollisionEnabled = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            isCollisionEnabled = !isCollisionEnabled;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isCollisionEnabled && other.tag != "PostProcessing")
+        {
+            SendMessage("StartDeathSequence");
+            Invoke("ReloadLevel", loadLevelDelay);
+        }
+        explosionSFX.gameObject.SetActive(true);     
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
---
+++ Argon Assault/Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RS.Control;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasController : MonoBehaviour
+{
+    void OnEnable()
+    {
+        //Enemy.OnEnemyDeath += EnableAnimation;
+    }
+
+    private void OnDestroy()
+    {
+        //Enemy.OnEnemyDeath -= EnableAnimation;
+    }
+
+    public void EnableAnimation(bool enableCanvas)
+    {   
+        if(enableCanvas)
+        {
+            Time.timeScale = 0.4f;
+            GetComponent<Animator>().SetTrigger("enableFade");
+        }
+    }
+
+    public void LoadNextMap()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
