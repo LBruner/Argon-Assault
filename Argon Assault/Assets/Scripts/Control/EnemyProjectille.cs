12e0d1cd4b2390bf853099a19b6efd81898059b6
LBruner <leandrobruner15@gmail.com>
2021-07-14 16:07:30 -0300

    add enemy attack system & new UI

---
+++ Argon Assault/Assets/Scripts/Control/EnemyProjectille.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectille : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private bool chaseTarget = true;
+
+    private Vector3 target;
+    private Transform playerTargetTransform;
+
+
+    void Start()
+    {
+        playerTargetTransform = GameObject.FindWithTag("Target").GetComponent<Transform>();
+        target = playerTargetTransform.position;
+    }
+
+    void Update()
+    {
+        if(chaseTarget)
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>().position;
+            
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(90,90, 0);
+    }
+}
--- Argon Assault/Assets/Scripts/Core/PlayerHealth.cs
+++ Argon Assault/Assets/Scripts/Core/PlayerHealth.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CollisionHandler : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     [Tooltip("In seconds")][SerializeField] float loadLevelDelay;
 
     [Tooltip("Gameobject")][SerializeField] GameObject explosionSFX;
 
+    [SerializeField] int healthPoints = 3;
+
+    public static Action<int> OnDamageTaken;
     bool isCollisionEnabled = true;
 
     void Update()
@@ -19,11 +23,18 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isCollisionEnabled && other.tag != "PostProcessing" && other.tag != "PowerUp")
+        if (isCollisionEnabled && !other.CompareTag("PowerUp"))
         {
-            SendMessage("StartDeathSequence");
-            Invoke("ReloadLevel", loadLevelDelay);
-            explosionSFX.gameObject.SetActive(true);
+            healthPoints = Mathf.Max(healthPoints - 1, 0);
+
+            OnDamageTaken?.Invoke(healthPoints);
+            
+            if(healthPoints == 0)
+            {
+                SendMessage("StartDeathSequence");
+                Invoke("ReloadLevel", loadLevelDelay);
+                explosionSFX.gameObject.SetActive(true);
+            }
         }
     }
 
